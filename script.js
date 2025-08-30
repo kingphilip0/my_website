86274e595761fc6e13aec4cca1b55c1edfeae6fe
kingphilip0 <olagunjutunmise03@gmail.com>
2025-08-30 23:28:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dark Mode Toggle
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+// Welcome Alert on Homepage
+function welcomeMessage() {
+  alert("👋 Welcome to my website! Explore and enjoy.");
+}
